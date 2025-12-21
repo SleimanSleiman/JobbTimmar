fe8e7e9c7618e2bfe8143be36d9bc4cb6ede930f
Sleiman Sleiman <Sleimanx2@gmail.com>
2025-12-21 18:35:47 +0100

    Update secrets.dart

--- lib/config/secrets.dart
+++ lib/config/secrets.dart
@@ -1,1 +1,7 @@
+/// API-nycklar och hemligheter
+/// OBS: Lägg INTE till denna fil i version control (git)
+/// Lägg till 'lib/config/secrets.dart' i .gitignore
 
+class Secrets {
+  static const String geminiApiKey = 'AIzaSyApg2VT5YdZSY63niDcyv0X2nJE-MVSgNc';
+}
